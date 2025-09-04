5a5f6d0b1e2f8230ebe48b63bd30a0436b4f3e13
Dan Price <danprice@HNLT-6.local>
2025-09-04 09:22:36 +0100

    chore: add remotePatterns for next/image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here */images: {
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "cohortai.co",
+      pathname: "/wp-content/**",
+    },
+    {
+      protocol: "https",
+      hostname: "placehold.co",
+      pathname: "/**",
+    },
+  ],
+},
+
 };
 
 export default nextConfig;
